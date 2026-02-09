4726de76098ca855676f42e51be0b7f3cdffefc8
Ansh Tulsyan <atulsyan850@gmail.com>
2026-02-09 12:52:36 -0600

    Improve responsive layout breakpoints

--- blog/src/template/PageWrapper.jsx
+++ blog/src/template/PageWrapper.jsx
@@ -9,8 +9,8 @@ export default function PageWrapper({ children }) {
       <Header />
 
       <main className="flex-1">
-        <div className="mx-auto max-w-[1180px] px-6 pt-28 pb-16 flex flex-col gap-10 overflow-visible" style={{ display: 'grid', gridTemplateColumns: '220px minmax(0, 1fr)', gap: '2.5rem', alignItems: 'start' }}>
-          <div className="hidden xl:block -ml-6 sticky top-5">
+        <div className="mx-auto max-w-[1180px] px-6 pt-28 pb-16 flex flex-col gap-10 overflow-visible lg:grid lg:grid-cols-[220px_minmax(0,_1fr)] lg:items-start">
+          <div className="hidden lg:block -ml-6 sticky top-5">
             <Toc />
           </div>
           <article className="lw-content max-w-3xl flex-1 prose prose-lg" style={{ '--tw-prose-headings': 'hsl(220 25% 12%)' }}>
